b3c715543ffd74a29b56cbf47d7d458f0ad484db
Pascal Audet <pascal.audet@uottawa.ca>
2019-12-14 22:18:16 -1000

    PEP8 sks_split

--- Scripts/sks_split.py
+++ Scripts/sks_split.py
@@ -11,8 +11,8 @@
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -22,6 +22,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+from obspy.clients.fdsn import Client
+from obspy.taup import TauPyModel
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication
+import matplotlib.pyplot as plt
 """
 Program sks_split.py
 --------------------
@@ -161,17 +167,12 @@ import os.path
 import numpy as np
 import matplotlib
 matplotlib.use('Qt5Agg')
-import matplotlib.pyplot as plt
 
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSlot
 
 # Import Obspy Modules
-from obspy.taup import TauPyModel
-from obspy.clients.fdsn import Client
 
 # Main function
+
 def main():
 
     app = QApplication([])
@@ -216,7 +217,8 @@ def main():
         if len(opts.UserAuth) == 0:
             client = Client(opts.Server)
         else:
-            client = Client(opts.Server, user=opts.UserAuth[0], password=opts.UserAuth[1])
+            client = Client(
+                opts.Server, user=opts.UserAuth[0], password=opts.UserAuth[1])
 
         # Get catalogue search start time
         if opts.startT is None:
@@ -234,9 +236,11 @@ def main():
 
         # Temporary print locations
         tlocs = sta.location
-        if len(tlocs) == 0: tlocs = ['']
+        if len(tlocs) == 0:
+            tlocs = ['']
         for il in range(0, len(tlocs)):
-            if len(tlocs[il]) == 0: tlocs[il] = "--"
+            if len(tlocs[il]) == 0:
+                tlocs[il] = "--"
         sta.location = tlocs
 
         # Update Display
@@ -244,48 +248,67 @@ def main():
         print(" ")
         print("|===============================================|")
         print("|===============================================|")
-        print("|                   {0:>8s}                    |".format(sta.station))
+        print("|                   {0:>8s}                    |".format(
+            sta.station))
         print("|===============================================|")
         print("|===============================================|")
-        print("|  Station: {0:>2s}.{1:5s}                            |".format(sta.network, sta.station))
-        print("|      Channel: {0:2s}; Locations: {1:15s}  |".format(sta.channel, ",".join(tlocs)))
-        print("|      Lon: {0:7.2f}; Lat: {1:6.2f}                |".format(sta.longitude, sta.latitude))
-        print("|      Start time: {0:19s}          |".format(sta.startdate.strftime("%Y-%m-%d %H:%M:%S")))
-        print("|      End time:   {0:19s}          |".format(sta.enddate.strftime("%Y-%m-%d %H:%M:%S")))
+        print("|  Station: {0:>2s}.{1:5s}                            |".format(
+            sta.network, sta.station))
+        print("|      Channel: {0:2s}; Locations: {1:15s}  |".format(
+            sta.channel, ",".join(tlocs)))
+        print("|      Lon: {0:7.2f}; Lat: {1:6.2f}                |".format(
+            sta.longitude, sta.latitude))
+        print("|      Start time: {0:19s}          |".format(
+            sta.startdate.strftime("%Y-%m-%d %H:%M:%S")))
+        print("|      End time:   {0:19s}          |".format(
+            sta.enddate.strftime("%Y-%m-%d %H:%M:%S")))
         print("|-----------------------------------------------|")
         print("| Searching Possible events:                    |")
-        print("|   Start: {0:19s}                  |".format(evst.strftime("%Y-%m-%d %H:%M:%S")))
-        print("|   End:   {0:19s}                  |".format(evet.strftime("%Y-%m-%d %H:%M:%S")))
+        print("|   Start: {0:19s}                  |".format(
+            evst.strftime("%Y-%m-%d %H:%M:%S")))
+        print("|   End:   {0:19s}                  |".format(
+            evet.strftime("%Y-%m-%d %H:%M:%S")))
         if opts.maxmag is None:
-            print("|   Mag:   >{0:3.1f}                                 |".format(opts.minmag))
+            print("|   Mag:   >{0:3.1f}                             " +
+                  "    |".format(
+                      opts.minmag))
         else:
-            print("|   Mag:   {0:3.1f} - {1:3.1f}                            |".format(opts.minmag, opts.maxmag))
+            print(
+                "|   Mag:   {0:3.1f} - {1:3.1f}                     " +
+                "       |".format(opts.minmag, opts.maxmag))
         print("| ...                                           |")
 
         # Get catalogue using deployment start and end
-        cat = client.get_events(starttime=evst, endtime=evet,    \
-                    minmagnitude=opts.minmag, maxmagnitude=opts.maxmag)
+        cat = client.get_events(
+            starttime=evst, endtime=evet,
+            minmagnitude=opts.minmag, maxmagnitude=opts.maxmag)
 
         # Total number of events in Catalogue
         nevK = 0
         nevtT = len(cat)
-        print("|  Found {0:5d} possible events                  |".format(nevtT))
+        print(
+            "|  Found {0:5d} possible events                  |".format(nevtT))
 
         # Get Local Data Availabilty
         if len(opts.localdata) > 0:
             print("|-----------------------------------------------|")
             print("| Cataloging Local Data...                      |")
             if opts.useNet:
-                stalcllist = splitpy.io.list_local_data_stn(lcldrs=opts.localdata, sta=sta.station, \
-                                 net=sta.network, altnet=sta.altnet)
-                print("|   {0:>2s}.{1:5s}: {2:6d} files                      |".format( \
-                    sta.network, sta.station, len(stalcllist)))
+                stalcllist = splitpy.io.list_local_data_stn(
+                    lcldrs=opts.localdata, sta=sta.station,
+                    net=sta.network, altnet=sta.altnet)
+                print("|   {0:>2s}.{1:5s}: {2:6d} files              " +
+                      "        |".format(
+                          sta.network, sta.station, len(stalcllist)))
             else:
-                stalcllist = splitpy.io.list_local_data_stn(lcldrs=opts.localdata, sta=sta.station)
-                print("|   {0:5s}: {1:6d} files                         |".format(sta.station, len(stalcllist)))
+                stalcllist = splitpy.io.list_local_data_stn(
+                    lcldrs=opts.localdata, sta=sta.station)
+                print("|   {0:5s}: {1:6d} files                      " +
+                      "   |".format(
+                          sta.station, len(stalcllist)))
         else:
             stalcllist = []
-        print("|===============================================|")      
+        print("|===============================================|")
 
         # Select order of processing
         if opts.reverse:
@@ -308,7 +331,8 @@ def main():
             hr = str(split.meta.time.hour).zfill(2)
 
             # If distance between 85 and 120 deg:
-            if (split.meta.gac > opts.mindist and split.meta.gac < opts.maxdist):
+            if (split.meta.gac > opts.mindist and
+                    split.meta.gac < opts.maxdist):
 
                 # Display Event Info
                 nevK = nevK + 1
@@ -318,15 +342,25 @@ def main():
                     inum = nevtT - iev + 1
                 print(" ")
                 print("****************************************************")
-                print("* #{0:d} ({1:d}/{2:d}):  {3:13s}".format(nevK, inum, nevtT, split.meta.time.strftime("%Y%m%d_%H%M%S")))
-                print("*   Origin Time: " + split.meta.time.strftime("%Y-%m-%d %H:%M:%S"))
-                print("*   Lat: {0:6.2f}; Lon: {1:7.2f}".format(split.meta.lat, split.meta.lon))
-                print("*   Dep: {0:6.2f}; Mag: {1:3.1f}".format(split.meta.dep/1000., split.meta.mag))
-                print("*     {0:5s} -> Ev: {1:7.2f} km; {2:7.2f} deg; {3:6.2f}; {4:6.2f}".format(\
-                    split.sta.station, split.meta.epi_dist, split.meta.gac, split.meta.baz, split.meta.az))
+                print("* #{0:d} ({1:d}/{2:d}):  {3:13s}".format(
+                    nevK, inum, nevtT,
+                    split.meta.time.strftime("%Y%m%d_%H%M%S")))
+                print("*   Origin Time: " +
+                      split.meta.time.strftime("%Y-%m-%d %H:%M:%S"))
+                print(
+                    "*   Lat: {0:6.2f}; Lon: {1:7.2f}".format(
+                        split.meta.lat, split.meta.lon))
+                print(
+                    "*   Dep: {0:6.2f}; Mag: {1:3.1f}".format(
+                        split.meta.dep/1000., split.meta.mag))
+                print("*     {0:5s} -> Ev: {1:7.2f} km; {2:7.2f} deg; " +
+                      "{3:6.2f}; {4:6.2f}".format(
+                          split.sta.station, split.meta.epi_dist,
+                          split.meta.gac, split.meta.baz, split.meta.az))
 
                 # Get Travel times (Careful: here dep is in meters)
-                tt = tpmodel.get_travel_times(distance_in_degree=split.meta.gac, \
+                tt = tpmodel.get_travel_times(
+                    distance_in_degree=split.meta.gac,
                     source_depth_in_km=split.meta.dep/1000.)
 
                 # Loop over all times in tt
@@ -338,40 +372,48 @@ def main():
                         # Add SKS phase to Split object
                         split.add_phase(t, opts.vp)
 
-                        # Break out of loop 
+                        # Break out of loop
                         break
 
                 # Get data
-                split.get_data_NEZ(client=client, dts=opts.dts, stdata=stalcllist, ndval=opts.ndval)
+                split.get_data_NEZ(client=client, dts=opts.dts,
+                                   stdata=stalcllist, ndval=opts.ndval)
 
-                if split.err: continue
+                if split.err:
+                    continue
 
                 # Rotate from ZEN to LQT (Longitudinal, Radial, Transverse)
                 split.rotate_ZEN_LQT()
 
-                # Calculate snr over 30 seconds 
+                # Calculate snr over 30 seconds
                 split.calc_snrq(dt=30.)
 
                 # Make sure no processing happens for NaNs
-                if np.isnan(split.snrq): 
+                if np.isnan(split.snrq):
                     print("* SNR NaN...Skipping")
-                    print("****************************************************")
-                    continue 
+                    print("*********************************************" +
+                          "*******")
+                    continue
 
                 # SNR below threshold
                 elif split.snrq < opts.msnr:
-                    print("* SNR Failed: {0:.2f} < {1:.2f}...Skipping".format(split.snrq, opts.msnr))
-                    print("****************************************************")
+                    print(
+                        "* SNR Failed: {0:.2f} < {1:.2f}...Skipping".format(
+                            split.snrq, opts.msnr))
+                    print("*********************************************" +
+                          "*******")
 
                 # If SNR is higher than threshold
                 elif split.snrq >= opts.msnr:
-                    print("* SNR Passed: {0:4.2f} >= {1:3.1f}".format(split.snrq, opts.msnr))
+                    print(
+                        "* SNR Passed: {0:4.2f} >= {1:3.1f}".format(
+                            split.snrq, opts.msnr))
 
                     # Output file
-                    outfile = outdir + '/Split' + '.' + split.sta.station + '.' + \
-                                    split.meta.time.strftime("%Y.%j.%H%M%S") + '.pkl'
-                    outfig = outdir + '/Split' + '.' + split.sta.station + '.' + \
-                                    split.meta.time.strftime("%Y.%j.%H%M%S") + '.png'
+                    outfile = outdir + '/Split' + '.' + split.sta.station +  \
+                        '.' + split.meta.time.strftime("%Y.%j.%H%M%S") + '.pkl'
+                    outfig = outdir + '/Split' + '.' + split.sta.station + \
+                        '.' + split.meta.time.strftime("%Y.%j.%H%M%S") + '.png'
 
                     # Check if result exists already
                     splitExists = False
@@ -380,19 +422,24 @@ def main():
 
                     # Should we Re-Pick
                     if splitExists:
-                        if (not opts.ovr and not opts.skip) or (opts.ovr and opts.skip):
+                        if ((not opts.ovr and not opts.skip) or
+                                (opts.ovr and opts.skip)):
                             repeat = Repeat()
                             if not repeat.reply:
-                            # if not splitpy.gui.repeat():
+                                # if not splitpy.gui.repeat():
                                 print("* Split Results Exist --> Skipping")
-                                print("****************************************************")
+                                print(
+                                    "**************************************" +
+                                    "**************")
                                 continue
                             else:
                                 print("* Split Results Exist --> Repeating")
 
                         elif not opts.ovr and opts.skip:
                             print("* Split Results Exist --> Skipping")
-                            print("****************************************************")
+                            print(
+                                "*******************************************" +
+                                "*********")
                             continue
                         elif opts.ovr and not opts.skip:
                             print("* Split Results Exist --> Overwriting")
@@ -403,7 +450,8 @@ def main():
                     # Continue if problem with analysis
                     if split.RC_res.edtt is None or split.SC_res.edtt is None:
                         print("* !!! DOF Error. --> Skipping...")
-                        print("****************************************************")
+                        print("*********************************************" +
+                              "*******")
                         continue
 
                     # Determine is Null and quality of estimate
@@ -439,14 +487,16 @@ def main():
                             # Extract times from clicks
                             tp1 = [xx for xx, yy in xc][0]
                             tp2 = [xx for xx, yy in xc][1]
-                            
+
                             # Make sure tp2 > tp1
                             if tp2 < tp1:
                                 tp11 = tp2
                                 tp2 = tp1
                                 tp1 = tp11
 
-                            print("*   Time Range: {0:.2f}, {1:.2f}".format(tp1, tp2))
+                            print(
+                                "*   Time Range: {0:.2f}, {1:.2f}".format(
+                                    tp1, tp2))
 
                             # Re-define time window
                             t1 = split.meta.time + split.ts + tp1
@@ -459,12 +509,16 @@ def main():
                             split.analyze(t1=t1, t2=t2)
 
                             # Check for fault result
-                            if split.RC_res.edtt is None or split.SC_res.edtt is None:
+                            if (split.RC_res.edtt is None or
+                                    split.SC_res.edtt is None):
                                 print("* !!! DOF Error. --> Skipping...")
-                                print("****************************************************")
+                                print(
+                                    "**************************************" +
+                                    "**************")
                                 continue
 
-                            # Determine if estimate is Null and quality of estimate
+                            # Determine if estimate is Null and quality of
+                            # estimate
                             split.calc_snrt(t1, t2 - t1)
                             split.is_null(opts.snrTlim, 5)
                             split.get_quality(5)
@@ -506,14 +560,21 @@ def main():
 
                                     # Save diagnostic plot
                                     dplot.save(outfig)
-                                    
-                                    print("* Estimate Saved to File: " + outfile)
+
+                                    print("* Estimate Saved to File: " +
+                                          outfile)
                                 else:
-                                    print("* Existing Estimate Retained (" + outfile + ")")
-                                print("****************************************************")
+                                    print(
+                                        "* Existing Estimate Retained (" +
+                                        outfile + ")")
+                                print(
+                                    "************************************" +
+                                    "****************")
                             else:
                                 print("* Estimate Discarded ")
-                                print("****************************************************")
+                                print(
+                                    "*************************************" +
+                                    "***************")
                                 continue
 
 
