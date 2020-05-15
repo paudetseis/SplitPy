f35d4352e61e995a2213a65dddf63c2fc1f53014
Pascal Audet <pascal.audet@uottawa.ca>
2020-05-15 17:19:49 -0400

    upgraded setup with Path

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os.path
 from os import listdir
 import re
 from numpy.distutils.core import setup
+from pathlib import Path
 
 
 def find_version(*paths):
@@ -14,22 +15,24 @@ def find_version(*paths):
     raise RuntimeError("Unable to find version string.")
 
 
-scripts = ['Scripts/' + i for i in listdir('Scripts/')]
+scripts = [str(x) for x in Path('Scripts').iterdir() if x.is_dir()]
 
 setup(
     name='splitpy',
     version=find_version('splitpy', '__init__.py'),
     description='Software for teleseismic shear-wave splitting analysis',
     author='Pascal Audet, Andrew Schaeffer',
+    author_email='pascal.audet@uottawa.ca',
     maintainer='Pascal Audet, Andrew Schaeffer',
     maintainer_email='pascal.audet@uottawa.ca, andrew.schaeffer@canada.ca',
+    url='https://github.com/paudetseis/SplitPy',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'],
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'],
     install_requires=['stdb', 'obspy', 'dill', 'PyQt5'],
     python_requires='>=3.6',
     packages=['splitpy'],
-    scripts=scripts,
-    url='https://github.com/paudetseis/SplitPy')
+    scripts=scripts)
