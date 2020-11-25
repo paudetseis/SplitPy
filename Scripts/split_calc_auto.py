e1c64d40a8786007058b5b7df8a052363789851b
Pascal Audet <pascal.audet@uottawa.ca>
2020-11-25 12:28:36 -0500

    updated scripts to stdb=0.1.4

--- Scripts/split_calc_auto.py
+++ Scripts/split_calc_auto.py
@@ -42,20 +42,26 @@ def main():
     args = arguments.get_arguments_calc_auto()
 
     # Load Database
-    db = stdb.io.load_db(fname=args.indb)
+    # stdb=0.1.4
+    try:
+        db, stkeys = stdb.io.load_db(fname=args.indb, keys=args.stkeys)
 
-    # Construct station key loop
-    allkeys = db.keys()
-    sorted(allkeys)
+    # stdb=0.1.3
+    except:
+        db = stdb.io.load_db(fname=args.indb)
 
-    # Extract key subset
-    if len(args.stkeys) > 0:
-        stkeys = []
-        for skey in args.stkeys:
-            stkeys.extend([s for s in allkeys if skey in s])
-    else:
-        stkeys = db.keys()
-        sorted(stkeys)
+        # Construct station key loop
+        allkeys = db.keys()
+        sorted(allkeys)
+
+        # Extract key subset
+        if len(args.stkeys) > 0:
+            stkeys = []
+            for skey in args.stkeys:
+                stkeys.extend([s for s in allkeys if skey in s])
+        else:
+            stkeys = db.keys()
+            sorted(stkeys)
 
     # Loop over station keys
     for stkey in list(stkeys):
@@ -255,7 +261,7 @@ def main():
                     # Filter rotated traces
                     split.dataLQT.filter('bandpass', freqmin=args.fmin,
                                          freqmax=args.fmax)
-                    
+
                     # Calculate snr over dt_snr seconds
                     split.calc_snr()
 
