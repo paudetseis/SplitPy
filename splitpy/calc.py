fixed error estimation for error surface average

splitpy/calc.py
@@ -21,7 +21,9 @@
 # SOFTWARE.
 
 # -*- coding: utf-8 -*-
+import copy
 import numpy as np
+from scipy import stats
 from obspy.core import Trace, Stream
 from numpy.linalg import inv
 
@@ -321,7 +323,6 @@ def split_RotCorr(trQ, trT, baz, t1, t2, maxdt, ddt, dphi):
     return Cmap, trQ_c, trT_c, trFast, trSlow, \
         phiRC, dtRC, phiRC_max
 
-
 def tshift(trace, tt):
     """
     Shifts a :class:`~obspy.core.Trace` object
@@ -421,8 +422,6 @@ def split_errorSC(tr, t1, t2, q, Emat, maxdt, ddt, dphi):
 
     """
 
-    from scipy import stats
-
     # Bounds on search
     phi = np.arange(-90.0, 90.0, dphi)*np.pi/180.
     dtt = np.arange(0., maxdt, ddt)
@@ -445,8 +444,13 @@ def split_errorSC(tr, t1, t2, q, Emat, maxdt, ddt, dphi):
     err_contour = vmin*(1. + n_par/(dof - n_par) *
                         stats.f.ppf(1. - q, n_par, dof - n_par))
 
+    # Roll copy of Emat to center
+    ind = np.where(Emat == Emat.min())
+    ind_phi = ind[0][0]
+    E2 = np.roll(Emat, len(phi)//2-ind_phi, axis=0)
+
     # Estimate uncertainty (q confidence interval)
-    err = np.where(Emat < err_contour)
+    err = np.where(E2 < err_contour)
     if len(err) == 0:
         return False, False, False
     err_phi = max(
@@ -495,7 +499,6 @@ def split_errorRC(tr, t1, t2, q, Emat, maxdt, ddt, dphi):
         Error contour for plotting
 
     """
-    from scipy import stats
 
     phi = np.arange(-90.0, 90.0, dphi)*np.pi/180.
     dtt = np.arange(0., maxdt, ddt)
@@ -524,8 +527,13 @@ def split_errorRC(tr, t1, t2, q, Emat, maxdt, ddt, dphi):
     # Back transformation
     err_contour = np.tanh(zrr_contour)
 
+    # Roll copy of Emat to center
+    ind = np.where(Emat == Emat.min())
+    ind_phi = ind[0][0]
+    E2 = np.roll(Emat, len(phi)//2-ind_phi, axis=0)
+
     # Estimate uncertainty (q confidence interval)
-    err = np.where(Emat < err_contour)
+    err = np.where(E2 < err_contour)
     err_phi = max(
         0.25*(phi[max(err[0])] - phi[min(err[0])])*180./np.pi, 0.25*dphi)
     err_dtt = max(0.25*(dtt[max(err[1])] - dtt[min(err[1])]), 0.25*ddt)
@@ -561,8 +569,6 @@ def split_error_average(q, Emat, maxdt, ddt, dphi, n):
 
     """
 
-    from scipy import stats
-
     # Bounds on search
     phi = np.arange(-90.0, 90.0, dphi)*np.pi/180.
     dtt = np.arange(0., maxdt, ddt)
@@ -580,8 +586,13 @@ def split_error_average(q, Emat, maxdt, ddt, dphi, n):
         err_contour = Emin*(1. + n_par/(dof - n_par) *
                             stats.f.ppf(1. - q, n_par, dof - n_par))
 
+    # Roll copy of Emat to center
+    ind = np.where(Emat == Emat.min())
+    ind_phi = ind[0][0]
+    E2 = np.roll(Emat, len(phi)//2-ind_phi, axis=0)
+
     # Estimate uncertainty (q confidence interval)
-    err = np.where(Emat < err_contour)
+    err = np.where(E2 < err_contour)
     if len(err) == 0:
         return False, False, False
     err_phi = max(

splitpy/classes.py
@@ -623,7 +623,7 @@ class Split(object):
         nrms = np.sqrt(np.mean(np.square(trNzeT.data)))
         self.meta.snrt = 10*np.log10(srms*srms/nrms/nrms)
 
-    def analyze(self, t1=None, t2=None, verbose=False):
+    def analyze(self, t1=None, t2=None, maxdt=4., ddt=0.1, dphi=1., verbose=False):
         """
         Calculates the shear-wave splitting parameters based
         on two alternative method: the Rotation-Correlation (RC)
@@ -648,6 +648,10 @@ class Split(object):
 
         """
 
+        self.meta.maxdt = maxdt
+        self.meta.ddt = ddt
+        self.meta.dphi = dphi
+
         if t1 is None and t2 is None:
             t1 = self.meta.time + self.meta.ttime - 5.
             t2 = self.meta.time + self.meta.ttime + 25.
@@ -662,12 +666,12 @@ class Split(object):
         Emat, trQ_c, trT_c, trFast, trSlow, phi, dtt, phi_min = \
             calc.split_RotCorr(
                 trQ, trT, self.meta.baz, t1, t2,
-                self.meta.maxdt, self.meta.ddt, self.meta.dphi)
+                maxdt, ddt, dphi)
 
         # Calculate error
         edtt, ephi, errc = calc.split_errorRC(
             trT_c, t1, t2, 0.05, Emat,
-            self.meta.maxdt, self.meta.ddt, self.meta.dphi)
+            maxdt, ddt, dphi)
 
         # Store dictionary as attribute
         self.RC_res = Result(Emat, trQ_c, trT_c, trFast, trSlow,
@@ -679,12 +683,12 @@ class Split(object):
         Emat, trQ_c, trT_c, trFast, trSlow, phi, dtt, phi_min = \
             calc.split_SilverChan(
                 trQ, trT, self.meta.baz, t1, t2,
-                self.meta.maxdt, self.meta.ddt, self.meta.dphi)
+                maxdt, ddt, dphi)
 
         # Calculate errors
         edtt, ephi, errc = calc.split_errorSC(
             trT_c, t1, t2, 0.05, Emat,
-            self.meta.maxdt, self.meta.ddt, self.meta.dphi)
+            maxdt, ddt, dphi)
 
         self.SC_res = Result(Emat, trQ_c, trT_c, trFast, trSlow,
                              phi, dtt, phi_min, edtt, ephi, errc)
@@ -1486,8 +1490,10 @@ class DiagPlot(object):
 
         extent = [phi.min(), phi.max(), dt.min(), dt.max()]
         X, Y = np.meshgrid(dt, phi)
-        E2 = np.roll(self.split.RC_res.Emat, int(
-            self.split.RC_res.phi - self.split.RC_res.phi_min), axis=0)
+        E2 = np.roll(
+            self.split.RC_res.Emat,
+            int((self.split.RC_res.phi - self.split.RC_res.phi_min)/self.split.meta.dphi),
+            axis=0)
 
         Emin = self.split.RC_res.Emat.min()
         Emax = self.split.RC_res.Emat.max()
@@ -1545,8 +1551,10 @@ class DiagPlot(object):
         extent = [phi.min(), phi.max(), dt.min(), dt.max()]
         X, Y = np.meshgrid(dt, phi)
 
-        E2 = np.roll(self.split.SC_res.Emat, int(
-            self.split.SC_res.phi-self.split.SC_res.phi_min), axis=0)
+        E2 = np.roll(
+            self.split.SC_res.Emat,
+            int((self.split.SC_res.phi-self.split.SC_res.phi_min)/self.split.meta.dphi),
+            axis=0)
 
         Emin = self.split.SC_res.Emat.min()
         Emax = self.split.SC_res.Emat.max()

splitpy/scripts/split_average.py
@@ -439,7 +439,7 @@ def main(args=None):
 
         EmatRC_mean = 0.
         for E, pRC, pRC_min in zip(EmatRC, phiRC, phiRC_min):
-            E2 = np.roll(E, int(pRC - pRC_min), axis=0)
+            E2 = np.roll(E, int((pRC - pRC_min)/dphi), axis=0)
             EmatRC_mean += E2
 
         EmatRC_mean = np.array(EmatRC_mean)/len(EmatRC)
@@ -463,7 +463,7 @@ def main(args=None):
         # SC analysis
         EmatSC_mean = 0.
         for E, pSC, pSC_min in zip(EmatRC, phiSC, phiSC_min):
-            E2 = np.roll(E, int(pSC - pSC_min), axis=0)
+            E2 = np.roll(E, int((pSC - pSC_min)/dphi), axis=0)
             EmatSC_mean += E2
 
         EmatSC_mean = EmatSC_mean/len(EmatSC)
@@ -524,18 +524,18 @@ def main(args=None):
         outplot = plotdir / (stkey + args.NullName + \
             args.QualName + "_ES_results.png")
 
+        print()
+        print("*** Estimates from averaging {0} error surfaces ***".format(len(baz)))
         if args.verb:
-            print()
-            print("*** Estimates from averaging {0} error surfaces ***".format(len(baz)))
             print("   PHI (RC): {0:7.1f} d +/- {1:.2f}".format(phi_bestRC, err_phiRC))
             print("   DT (RC):    {0:5.1f} s +/- {1:.2f}".format(dtt_bestRC, err_dttRC))
             print("   PHI (SC): {0:7.1f} d +/- {1:.2f}".format(phi_bestSC, err_phiSC))
             print("   DT (SC):    {0:5.1f} s +/- {1:.2f}".format(dtt_bestSC, err_dttSC))
             print("   PHI (mean): {0:7.1f} d +/- {1:.2f}".format(PHI, dPHI))
             print("   DT (mean):    {0:5.1f} s +/- {1:.2f}".format(DT, dDT))
-            print("   Saved to: ")
-            print("      "+str(outavRC))
-            print("      "+str(outavSC))
+        print("   Saved to: ")
+        print("      "+str(outavRC))
+        print("      "+str(outavSC))
 
         # Save Plot
         plt.savefig(outplot)
@@ -575,10 +575,10 @@ def main(args=None):
         dtmax = ceil(max([max(dtRC), max(dtSC), 3]))
 
         # Convert RC results to floats
-        phi = np.array([float(i) for i in phiRC])
-        Dphi = np.array([float(i) for i in DphiRC])
-        dt = np.array([float(i) for i in dtRC])
-        Ddt = np.array([float(i) for i in DdtRC])
+        phi = np.array(phiRC).astype('float')#[float(i) for i in phiRC])
+        Dphi = np.array(DphiRC).astype('float')#[float(i) for i in DphiRC])
+        dt = np.array(dtRC).astype('float')#[float(i) for i in dtRC])
+        Ddt = np.array(DdtRC).astype('float')#[float(i) for i in DdtRC])
 
         # Calculate average and STD for RC technique
         meanphiRC, stdphiRC, meandtRC, stddtRC = angle_mean(dt, phi, Ddt, Dphi)
@@ -597,10 +597,10 @@ def main(args=None):
                  180., [0, meandtRC], 'b', linewidth=2)
 
         # Convert SC results to floats
-        phi = np.array([float(i) for i in phiSC])
-        Dphi = np.array([float(i) for i in DphiSC])
-        dt = np.array([float(i) for i in dtSC])
-        Ddt = np.array([float(i) for i in DdtSC])
+        phi = np.array(phiSC).astype('float')#[float(i) for i in phiSC])
+        Dphi = np.array(DphiSC).astype('float')#[float(i) for i in DphiSC])
+        dt = np.array(dtSC).astype('float')#[float(i) for i in dtSC])
+        Ddt = np.array(DdtSC).astype('float')#[float(i) for i in DdtSC])
 
         # Calculate average and STD for SC technique
         meanphiSC, stdphiSC, meandtSC, stddtSC = angle_mean(dt, phi, Ddt, Dphi)
@@ -712,24 +712,25 @@ def main(args=None):
         ax3.axhline(DT + dDT, c='coral', linestyle='--',
                     linewidth=1, alpha=0.5)
 
+        print()
+        print(
+            "*** Estimates from averaging {0} individual measurements ***".format(
+                len(baz)))
         if args.verb:
-            print()
-            print(
-                "*** Estimates from averaging {0} individual measurements ***".format(
-                    len(baz)))
             print("   PHI (RC): {0:7.1f} d +/- {1:.2f}".format(meanphiRC, stdphiRC))
             print("   DT (RC):    {0:5.1f} s +/- {1:.2f}".format(meandtRC, stddtRC))
             print("   PHI (SC): {0:7.1f} d +/- {1:.2f}".format(meanphiSC, stdphiSC))
             print("   DT (SC):    {0:5.1f} s +/- {1:.2f}".format(meandtSC, stddtSC))
             print("   PHI (mean): {0:7.3f} d +/- {1:.3f}".format(PHI, dPHI))
             print("   DT (mean):    {0:5.3f} s +/- {1:.3f}".format(DT, dDT))
-            print("   Saved to: ")
-            print("      "+str(outavRC))
-            print("      "+str(outavSC))
-            print()
-            print("*** Catalogue of events and individual results ***")
-            print("   Saved to: "+str(outev))
-            print()
+        print("   Saved to: ")
+        print("      "+str(outavRC))
+        print("      "+str(outavSC))
+        print()
+        print("*** Catalogue of events and individual results ***")
+        print("   Saved to: ")
+        print("      "+str(outev))
+        print()
 
         # Write out Final Results
         # Write out Final Results
@@ -751,12 +752,12 @@ def main(args=None):
         # Write out events
         fid = open(outev, 'w')
         fid.writelines(
-            "eq_time (UTC), eq_lon, eq_lat, eq_mag, PHI_RC (deg), " +
-            "dPHI_RC (deg), DT_RC (s), dDT_RC (s), " +
+            "eq_time (UTC), eq_lon, eq_lat, eq_mag, eq_baz, " +
+            "PHI_RC (deg), dPHI_RC (deg), DT_RC (s), dDT_RC (s), " +
             "PHI_SC (deg), dPHI_SC (deg), DT_SC (s), dDT_SC (s)\n")
         for i in range(len(evlon)):
-            line1 = "{0},{1:8.4f},{2:7.4f},{3:3.1f},".format(
-                    evtime[i], evlon[i], evlat[i], evmag[i])
+            line1 = "{0},{1:8.4f},{2:7.4f},{3:3.1f},{4:4.1f},".format(
+                    evtime[i], evlon[i], evlat[i], evmag[i], baz[i])
             line2 = "{0:7.3f},{1:7.3f},{2:5.3f},{3:5.3f},".format(
                     phiRC[i], DphiRC[i], dtRC[i], DdtRC[i])
             line3 = "{0:7.3f},{1:7.3f},{2:5.3f},{3:5.3f}\n".format(

splitpy/scripts/split_calc_auto.py
@@ -88,9 +88,7 @@ def get_arguments_calc_auto(argv=None):
         default=False,
         help="Force the overwriting of pre-existing Split results. " +
         "Default behaviour prompts for those that " +
-        "already exist. Selecting overwrite and skip (ie, both flags) " +
-        "negate each other, and both are set to " +
-        "false (every repeat is prompted). [Default False]")
+        "already exist. [Default False]")
     parser.add_argument(
         "--zcomp", 
         dest="zcomp",
@@ -98,16 +96,6 @@ def get_arguments_calc_auto(argv=None):
         default="Z",
         help="Specify the Vertical Component Channel Identifier. "+
         "[Default Z].")
-    parser.add_argument(
-        "--skip-existing",
-        action="store_true",
-        dest="skip",
-        default=False,
-        help="Skip any event for which existing splitting results are " +
-        "saved to disk. Default behaviour prompts for " +
-        "each event. Selecting skip and overwrite (ie, both flags) " +
-        "negate each other, and both are set to " +
-        "False (every repeat is prompted). [Default False]")
     parser.add_argument(
         "--calc",
         action="store_true",
@@ -418,11 +406,6 @@ def get_arguments_calc_auto(argv=None):
         else:
             args.userauth = [None, None]
 
-    # Check existing file behaviour
-    if args.skip and args.ovr:
-        args.skip = False
-        args.ovr = False
-
     # Check Datatype specification
     if args.dtype.upper() not in ['MSEED', 'SAC']:
         parser.error(
@@ -670,10 +653,6 @@ def main(args=None):
                     if np.sum([file.exists() for file in
                                [ZNEfile, metafile, stafile]]) < 3:
                         continue
-                    sta = pickle.load(open(stafile, "rb"))
-                    split = Split(sta, zcomp=args.zcomp)
-                    meta = pickle.load(open(metafile, "rb"))
-                    split.meta = meta
                     dataZNE = pickle.load(open(ZNEfile, "rb"))
                     split.dataZNE = dataZNE
 
@@ -750,7 +729,11 @@ def main(args=None):
                 if args.calc or args.recalc:
 
                     # Analyze
-                    split.analyze(verbose=args.verb)
+                    split.analyze(
+                        maxdt=args.maxdt,
+                        ddt=args.ddt,
+                        dphi=args.dphi,
+                        verbose=args.verb)
 
                     # Continue if problem with analysis
                     if split.RC_res.edtt is None or split.SC_res.edtt is None: